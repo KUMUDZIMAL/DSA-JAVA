a8ad13adb1b73e97941c2bf453f504dfaff9b8b2
KUMUDZIMAL <kumud.223133206@vcet.edu.in>
2026-01-03 22:18:11 +0530

    done

---
+++ Binary Search Trees/Inorder Predecessor & Successor/code.java
@@ -0,0 +1,45 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        Node pre = null, suc = null;
+        Node curr = root;
+
+        // Find predecessor
+        while (curr != null) {
+            if (curr.data < key) {
+                pre = curr;
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+
+        curr = root;
+
+        // Find successor
+        while (curr != null) {
+            if (curr.data > key) {
+                suc = curr;
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+
+        ArrayList<Node> ans = new ArrayList<>();
+        ans.add(pre);
+        ans.add(suc);
+        return ans;
+    }
+}
+
+
