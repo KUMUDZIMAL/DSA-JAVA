92819f9cefd5af963b9a5e93c5526fbffeabff87
KUMUDZIMAL <kumud.223133206@vcet.edu.in>
2026-02-03 18:05:08 +0530

    done

---
+++ Greedy Algorithm/Assign Cookies/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n=g.length;
+        int m=s.length;
+        int l=0;
+        int r=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(l<m  && r < n){
+         if(s[l]>=g[r]){
+            r=r+1;
+         }
+         l=l+1;
+    
+        }
+             return r;
+    }}
