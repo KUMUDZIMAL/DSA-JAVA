b52b220ffc9d353ab9d243c36f786fded6c0e055
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-06-12 20:41:51 +0530

    done

--- DSA PATTERNS/Two Pointer Approach/Two Sum/Solution.java
+++ DSA PATTERNS/Two Pointer Approach/Two Sum/Solution.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -30,7 +31,7 @@ class Solution {
     }
 }
     // Brute Force Approach
-    
+
     //  int n = nums.length;
     //   for(int i=0;i<n-1;i++){
     //     for(int j=i+1;j<n;j++){
