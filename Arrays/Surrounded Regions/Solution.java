f00e5ef0d43289030a0e1248dff9be434b4fed14
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-06-20 23:34:13 +0530

    done

---
+++ Arrays/Surrounded Regions/Solution.java
@@ -0,0 +1,54 @@
+public class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, m - 1, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'E') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int r, int c) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') {
+            return;
+        }
+
+        board[r][c] = 'E';
+
+        dfs(board, r + 1, c);
+        dfs(board, r - 1, c);
+        dfs(board, r, c + 1);
+        dfs(board, r, c - 1);
+    }
+}
