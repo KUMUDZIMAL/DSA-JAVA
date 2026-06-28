f610ab6bfc4f6f1a0c482d52e79d8dac18005a51
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-06-28 17:26:02 +0530

    done

---
+++ Binary Trees/Path Sum - II/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+  class TreeNode {
+    int val;
+   TreeNode left;
+   TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+       this.val = val;
+      this.left = left;
+       this.right = right;
+     }
+ }
+ 
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(root, targetSum, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void backtrack(TreeNode root, int targetSum,
+                           List<Integer> path, List<List<Integer>> ans) {
+
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.val);
+
+        if (root.left == null && root.right == null && targetSum == root.val) {
+            ans.add(new ArrayList<>(path));
+        } else {
+            backtrack(root.left, targetSum - root.val, path, ans);
+            backtrack(root.right, targetSum - root.val, path, ans);
+        }
+
+        path.remove(path.size() - 1);
+    }
+}
