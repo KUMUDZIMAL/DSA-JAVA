65c2aa7fed0a563e576b24efad769f179564134e
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-08-06 11:49:04 +0530

    done

---
+++ Dynamic Programming/Delete Operation for Two Strings/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+
+        int m = word1.length();
+        int n = word2.length();
+
+        int[] prev = new int[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+
+            int[] curr = new int[n + 1];
+
+            for (int j = 1; j <= n; j++) {
+
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                }
+            }
+
+            prev = curr;
+        }
+
+        int lcs = prev[n];
+
+        return (m - lcs) + (n - lcs);
+    }
+}
