c1354c58f9d1fed12fca871ae4b9e11bf4153e58
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-08-18 11:07:48 +0530

    done

---
+++ Linked List/Linked List in Binary Tree/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+
+        if (root == null) {
+            return false;
+        }
+
+        
+        if (checkPath(head, root)) {
+            return true;
+        }
+
+        
+        return isSubPath(head, root.left) ||
+               isSubPath(head, root.right);
+    }
+
+    private boolean checkPath(ListNode head, TreeNode root) {
+
+        if (head == null) {
+            return true;
+        }
+
+        if (root == null) {
+            return false;
+        }
+
+        if (head.val != root.val) {
+            return false;
+        }
+
+        return checkPath(head.next, root.left) ||
+               checkPath(head.next, root.right);
+    }
+}
