3a871f3e12fcdf9359782801957bb3fdcc9357aa
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-08-19 15:13:30 +0530

    done

---
+++ Arrays/Cinema Seat Allocation/Solution.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            if (col >= 2 && col <= 9) {
+                map.computeIfAbsent(row, x -> new HashSet<>()).add(col);
+            }
+        }
+
+        int ans = (n - map.size()) * 2;
+
+        for (Set<Integer> reserved : map.values()) {
+
+            boolean left = true;
+            boolean middle = true;
+            boolean right = true;
+
+            for (int seat = 2; seat <= 5; seat++) {
+                if (reserved.contains(seat)) {
+                    left = false;
+                    break;
+                }
+            }
+
+            for (int seat = 4; seat <= 7; seat++) {
+                if (reserved.contains(seat)) {
+                    middle = false;
+                    break;
+                }
+            }
+
+            for (int seat = 6; seat <= 9; seat++) {
+                if (reserved.contains(seat)) {
+                    right = false;
+                    break;
+                }
+            }
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || middle || right) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+}
