55898607447ee3df1dac20716db46ff358a82ee8
KUMUDZIMAL <141753946+KUMUDZIMAL@users.noreply.github.com>
2026-09-02 10:14:12 +0530

    done

---
+++ Binary Search Trees/Unique Binary Search Trees II/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+ class TreeNode {
+      int val;
+      TreeNode left;
+     TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+ 
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return generate(1, n);
+    }
+
+    private List<TreeNode> generate(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        for (int rootVal = start; rootVal <= end; rootVal++) {
+            List<TreeNode> leftTrees = generate(start, rootVal - 1);
+            List<TreeNode> rightTrees = generate(rootVal + 1, end);
+
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(rootVal);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
